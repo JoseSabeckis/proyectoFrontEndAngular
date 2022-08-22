de6c1f644f9cedae3dff35ee204591b671452c51
JoseSabeckis <josesabeckis_pj@hotmail.com.ar>
2022-08-22 20:34:01 -0300

    imagenes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,20 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { AcercadeComponent } from './components/acercade/acercade.component';
+import { ExperienciaComponent } from './components/experiencia/experiencia.component';
+import { EducacionComponent } from './components/educacion/educacion.component';
+import { HardComponent } from './components/hard/hard.component';
+import { ProyectosComponent } from './components/proyectos/proyectos.component';
 
 @NgModule({
   declarations: [
     AppComponent,
+    AcercadeComponent,
+    ExperienciaComponent,
+    EducacionComponent,
+    HardComponent,
+    ProyectosComponent,
   ],
   imports: [
     BrowserModule
